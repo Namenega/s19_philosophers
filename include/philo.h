d47c3a203e0b91762a96de027d19d31701819004
namenega <mene.nath@protonmail.com>
2021-10-14 16:56:43 +0200

    Philo0.2 : add .h

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 16:03:28 by pyg               #+#    #+#             */
-/*   Updated: 2021/10/11 18:34:13 by namenega         ###   ########.fr       */
+/*   Updated: 2021/10/14 16:48:23 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,49 +20,85 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+# define ERR_MALLOC	"Error: allocation failed."
+# define DIED		"died."
+# define THINK		"is thinking."
+# define EAT		"is eating."
+# define LEFT_FORK	"has taken left fork."
+# define RIGHT_FORK	"has taken right fork."
+# define SLEEP		"is sleeping."
 typedef struct	s_philo
 {
-	int			num_philo;
-	int			num_forks;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			num_of_eat;
-	long int	start_time;
-	long int	actual_time;
-	pthread_t	*philo;
+	int				num_philo;
+	int				left_fork;
+	int				right_fork;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_of_eat;
+	int				death;			//do_we_have_a_dead
+	int				*philo_id;
+	int				*nb_meals;		// ate_enough
+	int				*meals;			// did_he_eat_enough
+	long int		start_time;	//start_timer
+	long int		*actual_time;	//when_did_he_eat
+	pthread_t		*philo;			//thread
+	pthread_t		thread_time;
+	pthread_mutex_t	*mutex;
+	pthread_mutex_t	write_mutex;
+	pthread_mutex_t	dead_mutex;
 }				t_philo;
 
 
-/*
-** MAIN_C
-*/
+/*********************************** MAIN_C ***********************************/
 
 int			main(int ac, char **av);
-void		error_msg(char *s);
 
-/*
-** PARSING_C
-*/
+/********************************** PARSER_C **********************************/
 
-int			parsing(t_philo *philo, char **av);
+int			parser_arg(int ac, char **av, t_philo *ph);
 
-/*
-** PHILO_C
-*/
+/********************************* PHILO_ID_C *********************************/
 
-int			philo(t_philo *ph);
-// void		*routine(void *arg);
+int			get_philo_id(t_philo *philo);
+
+/********************************** PHILO_C ***********************************/
+
+int			start(t_philo *philo);
+void		*routine(void *philo);
+
+/******************************* TIME_ROUTINE_C *******************************/
+
+void		*time_routine(void *time_arg);
 long int	what_time(t_philo *ph);
 
-/*
-** UTILS_0_C
-*/
+/********************************** EATING_C **********************************/
 
-int			ft_strlen(char *s);
-void		ft_bzero(void *s, size_t size);
-void		*ft_calloc(size_t size);
-int			ft_atoi(const char *nptr);
+void		eating(int curr_philo, t_philo *philo);
+
+/********************************* THINKING_C *********************************/
+
+void		thinking(int curr_philo, t_philo *philo);
+
+/********************************* SLEEPING_C *********************************/
+
+void		sleeping(int curr_philo, t_philo *philo);
+
+/********************************** UTILS_C ***********************************/
+
 t_philo		*get_struct(void);
+void		print_action(char *s, t_philo *philo, int curr_philo);
+int			ft_atoi(const char *nptr);
+int			error_msg(char *s);
+int			is_digit(char *s);
+
+
+
+
+
+
+
+
+
 
 #endif
