f190506027cf598d0998940e48ac498160b02bb7
namenega <mene.nath@protonmail.com>
2021-10-14 16:55:52 +0200

    Philo0.2 : main && parser

--- srcs/main.c
+++ srcs/main.c
@@ -6,38 +6,52 @@
 /*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 15:59:46 by pyg               #+#    #+#             */
-/*   Updated: 2021/10/11 18:36:15 by namenega         ###   ########.fr       */
+/*   Updated: 2021/10/14 16:52:15 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-// void	free_philo(t_philo *ph)
-// {
-// 	while (ph->p)
-// }
-
-void	error_msg(char *s)
+static void	free_philo(t_philo *philo)
 {
-	write(1, s, ft_strlen(s));
+	free(philo->philo_id);
+	free(philo->nb_meals);
+	free(philo->actual_time);
+	free(philo->meals);
+	free(philo->philo);
+	free(philo->mutex);
+}
+
+static void	initiate_struct(t_philo *ph)
+{
+	ph->num_philo = 0;
+	ph->left_fork = 0;
+	ph->right_fork = 0;
+	ph->time_to_die = 0;
+	ph->time_to_eat = 0;
+	ph->time_to_sleep = 0;
+	ph->num_of_eat = 0;
+	ph->death = 0;
+	ph->philo_id = NULL;
+	ph->nb_meals = NULL;
+	ph->meals = NULL;
+	ph->start_time = 0;
 }
 
 int main(int ac, char **av)
 {
-	t_philo	*ph;
+	t_philo	*philo;
 
-	ph = get_struct();
+	philo = get_struct();
+	initiate_struct(philo);
 	if (ac != 5 && ac != 6)
-	{
-		error_msg("ERROR: wrong number of ARGS");
-		return (0);
-	}
-	if (parsing(ph, av) == 1)
-		return (0);
-	ph->start_time = what_time(ph);
-	// printf("start_time = [%ld]\n", ph->start_time);
-	if (philo(ph) == -1)
+		return(error_msg("ERROR: wrong number of ARGS"));
+	if (parser_arg(ac, av, philo) == -1)
 		return (-1);
-	//system("leaks philo");
+	if (get_philo_id(philo) == -1)
+		return (-1);
+	if (start(philo) == -1)
+		return (-1);
+	free_philo(philo);
 	return (0);
 }
---
+++ srcs/parser.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/15 16:47:32 by pyg               #+#    #+#             */
+/*   Updated: 2021/10/14 16:34:36 by namenega         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+static int	parser_data(char **av, t_philo *data)
+{
+	data->num_philo = ft_atoi(av[1]);
+	if (data->num_philo < 1)
+		return (-1);
+	data->time_to_die = ft_atoi(av[2]);
+	if (data->time_to_die <= 0)
+		return (-1);
+	data->time_to_eat = ft_atoi(av[3]);
+	if (data->time_to_eat <= 0)
+		return (-1);
+	data->time_to_sleep = ft_atoi(av[4]);
+	if (data->time_to_sleep <= 0)
+		return (-1);
+	if (av[5])
+		data->num_of_eat = ft_atoi(av[5]);
+	else
+		data->num_of_eat = 0;
+	return (0);
+
+}
+
+int	parser_arg(int ac, char **av, t_philo *ph)
+{
+	int	i;
+
+	i = -1;
+	while (++i < ac)
+		if (is_digit(av[i]) == -1)
+			return (error_msg("Error: mistake in arguments."));
+	if (parser_data(av, ph) == -1)
+		return (-1);
+	return (0);
+}
